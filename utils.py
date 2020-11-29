cb77167a9b732296c113556d59d14701020fb599
sohaibl <sohaib@hotmail.fr>
2020-11-29 23:21:47 +0100

    update utils.py

--- utils.py
+++ utils.py
@@ -16,40 +16,48 @@ def get_palet(number=5):
 	return plot_colors
 
 
-def get_model_urls():
+def get_model_list():
 	return {
-		'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
-		'densenet121': 'https://download.pytorch.org/models/densenet121-a639ec97.pth',
-		#'densenet169': 'https://download.pytorch.org/models/densenet169-b2777c0a.pth',
-		'densenet169': 'https://download.pytorch.org/models/densenet169-6f0f7f60.pth',
-		'densenet201': 'https://download.pytorch.org/models/densenet201-c1103571.pth',
-		'densenet161': 'https://download.pytorch.org/models/densenet161-8d451a50.pth',
-		#  truncated _google to match module name
-		'inception_v3': 'https://download.pytorch.org/models/inception_v3_google-1a9a5a14.pth',
-		'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
-		'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
-		'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
-		'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
-		'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
-		'squeezenet1_0': 'https://download.pytorch.org/models/squeezenet1_0-a815701f.pth',
-		'squeezenet1_1': 'https://download.pytorch.org/models/squeezenet1_1-f364aa15.pth',
-		'vgg11': 'https://download.pytorch.org/models/vgg11-bbd30ac9.pth',
-		'vgg13': 'https://download.pytorch.org/models/vgg13-c768596a.pth',
-		'vgg16': 'https://download.pytorch.org/models/vgg16-397923af.pth',
-		'vgg19': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth',
-		'vgg11_bn': 'https://download.pytorch.org/models/vgg11_bn-6002323d.pth',
-		'vgg13_bn': 'https://download.pytorch.org/models/vgg13_bn-abd245e5.pth',
-		'vgg16_bn': 'https://download.pytorch.org/models/vgg16_bn-6c64b313.pth',
-		'vgg19_bn': 'https://download.pytorch.org/models/vgg19_bn-c79401a0.pth',
-	}
-
-
-def get_model_sizes():
-	return {
-		'alexnet': (224, 224),
-		'densenet': (224, 224),
-		'resnet': (224, 224),
-		'inception': (299, 299),
-		'squeezenet': (224, 224),  # not 255,255 acc. to https://github.com/pytorch/pytorch/issues/1120
-		'vgg': (224, 224)
+		'alexnet': {'url': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
+					'size': (224, 224)},
+		'densenet121': {'url': 'http://data.lip6.fr/cadene/pretrainedmodels/densenet121-fbdb23505.pth',
+			 'size': (224, 224)},
+		'densenet169': {'url': 'http://data.lip6.fr/cadene/pretrainedmodels/densenet169-f470b90a4.pth',
+			 'size': (224, 224)},
+		'densenet201': {'url': 'http://data.lip6.fr/cadene/pretrainedmodels/densenet201-5750cbb1e.pth',
+			 'size': (224, 224)},
+		'densenet161': {'url': 'http://data.lip6.fr/cadene/pretrainedmodels/densenet161-347e6b360.pth',
+			 'size': (224, 224)},
+		'inception_v3': {'url': 'https://download.pytorch.org/models/inception_v3_google-1a9a5a14.pth',
+			 'size': (299, 299)},
+		'resnet18': {'url': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+			 'size': (224, 224)},
+		'resnet34': {'url': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+			 'size': (224, 224)},
+		'resnet50': {'url': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+			 'size': (224, 224)},
+		'resnet101': {'url': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+			 'size': (224, 224)},
+		'resnet152': {'url': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+			 'size': (224, 224)},
+		'squeezenet1_0': {'url': 'https://download.pytorch.org/models/squeezenet1_0-a815701f.pth',
+			 'size': (224, 224)},
+		'squeezenet1_1': {'url': 'https://download.pytorch.org/models/squeezenet1_1-f364aa15.pth',
+			 'size': (224, 224)},
+		'vgg11': {'url': 'https://download.pytorch.org/models/vgg11-bbd30ac9.pth',
+			 'size': (224, 224)},
+		'vgg13': {'url': 'https://download.pytorch.org/models/vgg13-c768596a.pth',
+			 'size': (224, 224)},
+		'vgg16': {'url': 'https://download.pytorch.org/models/vgg16-397923af.pth',
+			 'size': (224, 224)},
+		'vgg19': {'url': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth',
+			 'size': (224, 224)},
+		'vgg11_bn': {'url': 'https://download.pytorch.org/models/vgg11_bn-6002323d.pth',
+			 'size': (224, 224)},
+		'vgg13_bn': {'url': 'https://download.pytorch.org/models/vgg13_bn-abd245e5.pth',
+			 'size': (224, 224)},
+		'vgg16_bn': {'url': 'https://download.pytorch.org/models/vgg16_bn-6c64b313.pth',
+			 'size': (224, 224)},
+		'vgg19_bn': {'url': 'https://download.pytorch.org/models/vgg19_bn-c79401a0.pth',
+			 'size': (224, 224)}
 	}
